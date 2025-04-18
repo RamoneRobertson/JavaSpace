9449f865652c583f57208e2b94de017fb05c4b03
Ramone Robertson <ramonerobertson10@gmail.com>
2025-04-18 15:17:31 +0900

    Finished try with resource block

--- src/main/java/exceptions/ExceptionDemo.java
+++ src/main/java/exceptions/ExceptionDemo.java
@@ -2,35 +2,22 @@ package exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class ExceptionDemo {
     public static void show(){
-        FileReader reader = null;
         // Example of a checked exception
         // FileNotFound Exception (The Java compiler checks for you at compile time)
-        try {
-            reader = new FileReader("file.txt");
+        try (
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("file.txt");
+        ){
             var value = reader.read();
-            reader.close();
-            new SimpleDateFormat().parse("");
-            System.out.println("File opened");
-        } catch(FileNotFoundException e){
-            System.out.println(e.getMessage());
-        } catch (IOException | ParseException e){
-            System.out.println(e.getMessage());
-        } finally {
-            if(reader != null){
-                try{
-                    reader.close();
-                }catch (IOException e){
-                    System.out.println(e.getMessage());
-                }
-
-            }
-
+        }  catch (IOException e){
+            System.out.println("Could not read file");
         }
 
         // Example of Unchecked exception
