2c9eafe1091d237b71ed31320a0f8ac17dff732c
Ramone Robertson <ramonerobertson10@gmail.com>
2025-04-18 15:08:32 +0900

    Handled IOException for File Reader

--- src/main/java/exceptions/ExceptionDemo.java
+++ src/main/java/exceptions/ExceptionDemo.java
@@ -1,17 +1,42 @@
 package exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionDemo {
     public static void show(){
+        FileReader reader = null;
         // Example of a checked exception
         // FileNotFound Exception (The Java compiler checks for you at compile time)
-        var reader = new FileReader("file.txt");
+        try {
+            reader = new FileReader("file.txt");
+            var value = reader.read();
+            reader.close();
+            new SimpleDateFormat().parse("");
+            System.out.println("File opened");
+        } catch(FileNotFoundException e){
+            System.out.println(e.getMessage());
+        } catch (IOException | ParseException e){
+            System.out.println(e.getMessage());
+        } finally {
+            if(reader != null){
+                try{
+                    reader.close();
+                }catch (IOException e){
+                    System.out.println(e.getMessage());
+                }
+
+            }
+
+        }
 
         // Example of Unchecked exception
         // This will throw a NullPointer Exception
         // Unchecked exceptions happen at runtime due to programming errors/mistakes
-        sayHello(null);
+//        sayHello(null);
     }
 
     public static void sayHello(String name) {
