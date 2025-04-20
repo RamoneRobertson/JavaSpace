f931025960f0743845519ca01d2db6cd9ef9da68
Ramone Robertson <ramonerobertson10@gmail.com>
2025-04-21 03:42:10 +0900

    Learning java

---
+++ src/main/java/generics/List.java
@@ -0,0 +1,14 @@
+package generics;
+
+public class List {
+    private int count;
+
+    public List(int size){
+        int[] data = new int[size];
+    }
+
+    public void add(int item){
+        
+    }
+
+}
