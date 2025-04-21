69843a6eac5200cf473629f401e573f96262134c
Ramone Robertson <ramonerobertson10@gmail.com>
2025-04-21 21:45:44 +0900

    Java generics lesson

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,12 @@
+import generics.GenericList;
+
 import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int randomNumber = new Random().nextInt(100);
-        int userGuess = 0;
-
-        while(userGuess != randomNumber){
-            System.out.println("Please enter a number between 1 and 100");
-            userGuess = scanner.nextInt();
-            System.out.println("You guessed: " + userGuess);
-            checkGuess(userGuess, randomNumber);
-        }
-
-
-    }
-
-    public static void checkGuess(int userGuess, int randomNumber) {
-        if(userGuess < randomNumber){
-            System.out.println("Your guess is too low");
-        } else if (userGuess > randomNumber) {
-            System.out.println("Your guess is too high");
-        } else if (userGuess == randomNumber) {
-            System.out.println("Bingo!");
-        }
+        new GenericList<>()
     }
 }
 
--- src/main/java/generics/GenericList.java
+++ src/main/java/generics/GenericList.java
@@ -1,4 +1,17 @@
 package generics;
 
-public class GenericList {
+// The T represents the type of objects we want to store
+public class GenericList <T> {
+//    private T[] items = new T[10];
+    // A better way to do this
+    private T[] items = (T[]) new Object[10];
+    private int count;
+
+    public void add(T item){
+        items[count++] = item;
+    }
+
+    public T get(int index){
+        return items[index];
+    }
 }
